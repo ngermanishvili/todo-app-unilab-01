3cb561ff37dd26d94b326c1f24eaf4359ab435c1
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-28 03:54:33 +0400

    Refactor TodoForm and TodoList components for improved code readability

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,55 +1,11 @@
+import {
+  StyledForm,
+  StyledInput,
+  StyledButton,
+  StyledTodoList,
+  StyledTodoRow,
+} from "./StyledComponents/TodoForm";
 import React, { useState } from "react";
-import styled from "styled-components";
-
-const StyledForm = styled.form`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-`;
-
-const StyledInput = styled.input`
-  background: #e6ebff;
-  outline: none;
-  max-width: 487px;
-  height: 76px;
-  width: 100%;
-  padding-left: 10px;
-  &::placeholder {
-    padding-left: 24px;
-  }
-`;
-
-const StyledButton = styled.button`
-  max-width: 108px;
-  height: 76px;
-  width: 100%;
-  margin-right: 5px;
-  padding: 1px;
-  border-color: white;
-  background-color: #5efc8d;
-`;
-
-const StyledTodoList = styled.div`
-  width: 595px;
-  height: 54px;
-`;
-
-const StyledTodoRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 4px auto;
-  color: white;
-  background: linear-gradient(
-    90deg,
-    rgba(20, 159, 255, 1) 0%,
-    rgba(17, 122, 255, 1) 100%
-  );
-  padding: 16px;
-  border-radius: 5px;
-  width: 90%;
-`;
 
 const TodoForm = ({ handleAddTodo }) => {
   const [input, setInput] = useState("");
