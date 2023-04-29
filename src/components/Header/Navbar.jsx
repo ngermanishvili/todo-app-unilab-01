a957c85179287ac4dde2ca95124a5b41044f14c7
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-29 20:34:10 +0400

    Added local storage for todos and styled component for the TodoContainer

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  // TODO Here i used the localStorage to save the name and image of the user.
+  const name = localStorage.getItem("name");
+  const image = localStorage.getItem("image");
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>TO DO</div>
       <div className={styles.rightContainer}>
-        <div className={styles.name}>Your name</div>
-        <div className={styles.circle}></div>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.circle}>
+          {image && <img src={image} alt="uploadedImg" />}
+        </div>
       </div>
     </nav>
   );
