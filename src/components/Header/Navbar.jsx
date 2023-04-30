9f01858ea29a1d4121066d9ae75044184ba00865
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-30 19:58:06 +0400

    Add dropdown and navbar to Header component and apply module CSS

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,18 +1,37 @@
+import { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
+import profile from "../../assets/profile.png";
+import DropdownMenu from "./DropDown";
 
 const Navbar = () => {
-  // TODO Here i used the localStorage to save the name and image of the user.
-  const name = localStorage.getItem("name");
-  const image = localStorage.getItem("image");
+  const [user, setUser] = useState({});
+  const [dropDown, setDropDown] = useState(false);
+
+  // Here I check if the user is authenticated, if yes, redirect to /todolist else, show the login page.
+  useEffect(() => {
+    const storedUser = localStorage.getItem("displayUser");
+    setUser(storedUser ? JSON.parse(storedUser) : {});
+  }, []);
+
+  const handleProfileCheck = () => {
+    setDropDown(!dropDown);
+  };
+
+ 
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>TO DO</div>
       <div className={styles.rightContainer}>
-        <div className={styles.name}>{name}</div>
-        <div className={styles.circle}>
-          {image && <img src={image} alt="uploadedImg" />}
+        <div className={styles.name}>{user.name}</div>
+        <div className={styles.circle} onClick={handleProfileCheck}>
+          {user.photo ? (
+            <img src={user.photo} alt="user" />
+          ) : (
+            <img src={profile} alt="user" />
+          )}
         </div>
+        {dropDown && <DropdownMenu />}
       </div>
     </nav>
   );
