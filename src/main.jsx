59dd53c3b8c4e173c0561c6fee6bb51204b9aa84
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-30 20:12:50 +0400

    Added Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './route/Home';
+import Login from './route/Login';
+import TodoList from './components/TodoList';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todolist" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
