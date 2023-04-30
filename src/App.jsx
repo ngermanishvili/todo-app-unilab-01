484e499dab3dc909a959ff468a269d8b26497507
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-30 20:13:36 +0400

    chore: cleaned up code formatting and removed unused variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,27 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./route/Home";
-import Login from "./route/Login";
-import TodoList from "./components/TodoList";
-
-// This Private route component is used to protect the /todolist route. If the user is not authenticated, they will be redirected to the /login route. Otherwise, they will be able to access the /todolist route. element: Element, isAuthenticated, ...rest are props passed to the component, and coming from the route definition. element is the component to render, isAuthenticated is a boolean indicating if the user is authenticated, and ...rest is the rest of the props passed to the component.
-
-const PrivateRoute = ({ element: Element, isAuthenticated, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      element={
-        isAuthenticated ? (
-          <Element />
-        ) : (
-          <Navigate to="/login" state={{ from: rest.location }} />
-        )
-      }
-    />
-  );
-};
+import Navbar from "/src/components/Header/Navbar.jsx";
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    localStorage.getItem("isAuthenticated") === "true"
+  // This is the state that will be used to check if the user is authenticated or not.
+  const [isAuthenticated, setAuthenticated] = useState(
+    localStorage.getItem("authenticated")
   );
 
-  // Here I check if the user is authenticated when the component is mounted. If yes, I set the isAuthenticated state to true. Otherwise, I set it to false.
   const handleLogin = () => {
-    setIsAuthenticated(true);
-    localStorage.setItem("isAuthenticated", "true"); // store the authentication status in local storage
-  };
-
-  // Here I set the isAuthenticated state to false and remove the authentication status from local storage. This will log the user out.
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    localStorage.setItem("isAuthenticated", "false"); // store the authentication status in local storage
+    setAuthenticated(true);
   };
 
   return (
     <BrowserRouter>
+    
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <PrivateRoute
-          path="/todolist"
-          element={<TodoList onLogout={handleLogout} />}
-          isAuthenticated={isAuthenticated}
-        />
+        {isAuthenticated ? (
+          <Route path="/todolist" element={<TodoList />} />
+        ) : (
+          <Navigate to="/login" />
+        )}
       </Routes>
     </BrowserRouter>
   );
