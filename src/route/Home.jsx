00f9d86621027f27f0f0d1817dc83943c10d5114
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-01 20:41:12 +0400

    Added Protected Route and comments

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -2,17 +2,25 @@ import {
   Wrapper,
   LogoImg,
 } from "../components/StyledComponents/Home/Home.styled";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Logo from "../assets/Group3.svg";
 
 const Home = () => {
+  // this hook is used to navigate between pages in the app (like a <Link> component) but it can be used anywhere in the app.
+  const navigate = useNavigate();
+
+  // This handleclick is for the button in the Home page. When the user clicks on the button, the user is redirected to the Login page.
+  const handleClick = () => {
+    navigate("/login");
+  };
+
   return (
     <Wrapper>
       <LogoImg src={Logo} />
       <h1>Keep Track Of Daily Tasks In Life</h1>
-      <Link className="btn" to="/login">
-        <span>Get Started</span>
-      </Link>
+      <button className="btn" onClick={handleClick}>
+        Get Started
+      </button>
     </Wrapper>
   );
 };
