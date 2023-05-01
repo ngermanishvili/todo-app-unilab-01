44364af31591cfacbd2257817bce364a282b0751
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-01 23:54:32 +0400

    chore: imported Protected routes from Protected Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from "./route/Home";
 import ProtectLoginRoute from "./components/Protected/ProtectLoginRoute";
 import ProtectTodoRoute from "./components/Protected/ProtectTodoRoute";
-//
+import ProtectedHome from "./components/Protected/ProtectedHome";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ProtectedHome />} />
         <Route path="/login" element={<ProtectLoginRoute />} />
         <Route path="/todolist" element={<ProtectTodoRoute />} />
       </Routes>
