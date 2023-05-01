799c190a0a11df9b77c395fe546a493c719c1636
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-01 23:10:16 +0400

    refactor: move protected routes to Protected folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,15 @@
-import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./route/Home";
-import Login from "./route/Login";
-import TodoList from "./components/TodoList";
+import ProtectLoginRoute from "./components/Protected/ProtectLoginRoute";
+import ProtectTodoRoute from "./components/Protected/ProtectTodoRoute";
 
 const App = () => {
-  // i'm using this state with using localStorage to check if the user is authenticated or not. If yes, the user is redirected to the TodoList page. If not, the user is redirected to the Login page.
-  const [isAuthenticated, setAuthenticated] = useState(
-    localStorage.getItem("authenticated")
-  );
-
-  const handleLogin = () => {
-    localStorage.setItem("authenticated", true);
-    setAuthenticated(true);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("authenticated");
-    setAuthenticated(false);
-  };
-
-  // in here i'm checking if the user is authenticated. If yes, the user is redirected to the TodoList page. If not, the user is redirected to the Login page.
-  const renderLoginRoute = () => {
-    if (isAuthenticated) {
-      return <Navigate to="/todolist" />;
-    } else {
-      return <Login onLogin={handleLogin} />;
-    }
-  };
-  // in here i'm check also if the user is authenticated. If yes, the user is redirected to the TodoList page. If not, the user is redirected to the Home page.
-  const renderTodoListRoute = () => {
-    if (isAuthenticated) {
-      return <TodoList onLogout={handleLogout} />;
-    } else {
-      return <Navigate to="/" />;
-    }
-  };
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={renderLoginRoute()} />
-        <Route path="/todolist" element={renderTodoListRoute()} />
+        <Route path="/login" element={<ProtectLoginRoute />} />
+        <Route path="/todolist" element={<ProtectTodoRoute />} />
       </Routes>
     </BrowserRouter>
   );
