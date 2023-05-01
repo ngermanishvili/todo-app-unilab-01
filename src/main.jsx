3fa92d9b43817bb64b9967446a09c4bd763a52f1
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-01 20:34:18 +0400

    Reorganize routes in App.jsx and remove from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Home from './route/Home';
-import Login from './route/Login';
-import TodoList from './components/TodoList';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/todolist" element={<TodoList />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
